d26ca1ae295a4f9162b77d820301a72f1a161471
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2022-05-16 09:55:56 +0300

    Conversions 1 & 2

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let tokens :Vec<&str> = s.split(",").collect();
+
+        if tokens.len() != 2 {
+            return Person::default();
+        }
+
+        let name :&str = tokens[0];
+        let age_str :&str = tokens[1];
+
+        if name.len() == 0 {
+            return Person::default();
+        }
+
+        if let Ok(age) = age_str.parse() {
+            return Person{name: name.to_string(), age }
+        } else {
+            return Person::default();
+        }
     }
 }
 
